2d9c51d40c946b2c1e92a806c96ae6ccefbcde4c
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2022-05-31 17:05:24 +0530

    Refined the descriptions of tests

--- test/testCircle.js
+++ test/testCircle.js
@@ -9,13 +9,14 @@ describe('Circle', () => {
     assert.ok(circle1.equals(circle2));
   });
 
-  it('should return false if instances are with different properties', () => {
-    const circle1 = new Circle(new Point(2, 3), 5);
-    const circle2 = new Circle(new Point(0, 0), 4);
-    assert.ok(!circle1.equals(circle2));
-  });
+  it('should not equate two instances of same circle with different properties',
+    () => {
+      const circle1 = new Circle(new Point(2, 3), 5);
+      const circle2 = new Circle(new Point(0, 0), 4);
+      assert.ok(!circle1.equals(circle2));
+    });
 
-  it('should return false if instances are of different class', () => {
+  it('should not equate two instances of different circle', () => {
     const circle1 = new Circle(new Point(10, -10), 5);
     const circle2 = { center: new Point(10, -10), radius: 5 };
     assert.ok(!circle1.equals(circle2));
