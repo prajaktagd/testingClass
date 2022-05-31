848af6defca6c977991f205a069d938ad4bacc8d
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2022-05-31 17:14:18 +0530

    Extracted square and round functions

--- src/circle.js
+++ src/circle.js
@@ -1,3 +1,8 @@
+const square = (number) => Math.pow(number, 2);
+
+const round = (number, numOfDecimals) =>
+  parseFloat(number.toFixed(numOfDecimals));
+
 class Circle {
   constructor(center, radius) {
     this.center = center;
@@ -11,8 +16,8 @@ class Circle {
   }
 
   area() {
-    const area = Math.PI * Math.pow(this.radius, 2);
-    return +area.toFixed(2);
+    const area = Math.PI * square(this.radius);
+    return round(area, 2);
   }
 }
 
